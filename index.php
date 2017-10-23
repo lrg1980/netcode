47770a6c1db6cf408c85865b58673d3922be3401
Leonardo Grabow <admin@binweb.net>
2017-10-23 10:08:29 -0300

    Pagina Login y Register

---
+++ index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Test Columnas</title>
+    <link rel="stylesheet" href="css/style.css">
+    <!-- Bootstrap Links -->
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css">
+
+    <!-- Latest compiled and minified JavaScript -->
+   <!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>-->
+    <!-- JavaScript de SlideShow-->
+    <script type="text/javascript " src="http://code.jquery.com/jquery-1.6.3.min.js "></script>
+    <script type="text/javascript " src="js/jquery.cycle.all.2.74.js "></script>
+    <script type="text/javascript" src="js/script.js"></script>
+</head>
+<body>
+    <<main>
+       <!-- Inicia Slideshow Background -->
+        <div id="slideshow">
+        <img src="images/1.png" class="bgM" />
+        <img src="images/2.png" class="bgM" />
+        <img src="images/3.png" class="bgM" />
+        <img src="images/4.png" class="bgM" />
+        <img src="images/5.png" class="bgM" />
+    </div>
+       <!-- Finaliza SlideShow Background -->
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                     <hgroup>
+                            <img src="images/isotipo_320x220_transparente.png" alt="isotipo" width="320px" height="220px">
+                            <!-- <h1>Red social de Coding</h1>
+                            <h3>By Leonardo Grabow</h3>-->
+                        </hgroup>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                     <form>
+                    <h2>Ingresar a la red de Coding</h2>
+                    <div class="group">
+                        <input type="text"><span class="highlight"></span><span class="bar"></span>
+                        <label>Nombre</label>
+                    </div>
+                    <div class="group">
+                        <input type="email"><span class="highlight"></span><span class="bar"></span>
+                        <label>Correo Electrónico</label>
+                    </div>
+                    <button type="button" class="button buttonBlue">Ingresar
+                        <div class="ripples buttonRipples"><span class="ripplesCircle"></span></div>
+                    </button>
+                </form>
+                </div>
+                <div class="col-md-6">
+                     <form>
+                    <h2>Registrarse a la red de Coding</h2>
+                    <div class="group">
+                        <input type="text"><span class="highlight"></span><span class="bar"></span>
+                        <label>Nombre</label>
+                    </div>
+                    <div class="group">
+                        <input type="email"><span class="highlight"></span><span class="bar"></span>
+                        <label>Correo Electrónico</label>
+                    </div>
+                    <button type="button" class="button buttonBlue">Ingresar
+                        <div class="ripples buttonRipples"><span class="ripplesCircle"></span></div>
+                    </button>
+                </form>
+                </div>
+            </div>
+        </div>
+    </main>
+
+</body>
+</html>
